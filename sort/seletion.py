2c2389b15fa94f4b96eeee68ecf20fb1fafac7a4
Serojisa551 <116907048+Serojisa551@users.noreply.github.com>
2022-11-08 21:55:07 +0400

    Add funcion creating_list

--- sort/seletion.py
+++ sort/seletion.py
@@ -1,9 +1,13 @@
 from random import randint
-lst=[]
+lst = []
 
-for i in range(randint(2, 10)):
-    lst.append(randint(0, 10))
-print("before", lst)
+
+def creating_list():
+    lst = []
+    for i in range(randint(2, 10)):
+        lst.append(randint(0, 10))
+    print("before", lst)
+    return selection(lst)
 
 
 def selection(lst):
@@ -11,8 +15,10 @@ def selection(lst):
         modul = i
         for j in range(i + 1, len(lst)):
             if lst[modul] > lst[j]:
-              modul = j
-        lst[i],lst[modul] = lst[modul], lst[i]  
+                modul = j
+        lst[i], lst[modul] = lst[modul], lst[i]
+    print("after",end="" )
     return lst
 
-print("after", selection(lst))	    	
+
+print(creating_list())
