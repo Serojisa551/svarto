a164f7922fb2ab4569381c22a0ff1a97c1a2339f
Serojisa551 <116907048+Serojisa551@users.noreply.github.com>
2022-11-08 21:43:43 +0400

    Add funcion creating_list

--- sort/bubble.py
+++ sort/bubble.py
@@ -1,21 +1,20 @@
 from random import randint
-lst=[]
-for i in range(randint(2, 10)):
-   lst.append(randint(0, 10))
-print("before",lst)
+
+def creating_list():
+    lst=[]
+    for i in range(randint(2, 10)):
+        lst.append(randint(0, 10))
+    print("before", lst)
+    return  bubbel(lst)
 
 
 def bubbel(ite):
-    for i in range(len(lst)):
-        is_assorted = True
-        for j in range(len(lst)):
-    	    if(lst[i] < lst[j]):
-               lst[i],lst[j] = lst[j],lst[i]
-              # is_assorted = False
-        #if is_assorted == True:
-         #  return "mtav",lst
-    return  lst
-
-
-print("after", bubbel(lst))
+    for i in range(len(ite)):
+        for j in range(len(ite)):
+    	    if(ite[i] < ite[j]):
+               ite[i],ite[j] = ite[j],ite[i]
+    print("after", end=" " )
+    return  ite
+if __name__ == "__main__":
+    print(creating_list())
 
