0f53ad5c05127039760241162fbe30333702151d
Serojisa551 <116907048+Serojisa551@users.noreply.github.com>
2022-11-08 21:51:41 +0400

    Add funcion creating_list

--- sort/inserion.py
+++ sort/inserion.py
@@ -1,17 +1,21 @@
 from random import randint
-lst=[]
 
-for i in range(randint(2, 10)):
-    lst.append(randint(0, 10))
-print("before", lst)
+
+def creating_list():
+    lst = []
+    for i in range(randint(2, 10)):
+        lst.append(randint(0, 10))
+    print("before", lst)
+    return inserion(lst)
 
 
 def inserion(ite):
-   for i in range(1, len(ite)):
-       for j in range(i, 0 , -1):
-          if ite[j] <ite[j - 1]:
-             ite[j], ite[j-1] = ite[j-1], ite[j] 
-                
-   return ite
+    for i in range(1, len(ite)):
+        for j in range(i, 0, -1):
+            if ite[j] < ite[j - 1]:
+                ite[j], ite[j-1] = ite[j-1], ite[j]
+    print("after", end="")
+    return ite
 
-print("after", inserion(lst))
+
+print(creating_list())
