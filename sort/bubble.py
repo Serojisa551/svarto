3ac99a936484c144a2568c89f8bb63a774dbc350
Serojisa551 <116907048+Serojisa551@users.noreply.github.com>
2022-11-12 17:43:43 +0400

    From the file bubble.py , removed personal function calls.

--- sort/bubble.py
+++ sort/bubble.py
@@ -1,21 +1,23 @@
 from random import randint
 from insertion import my_print
 
+
 def creating_list():
-    lst=[]
+    lst = []
     for i in range(randint(2, 10)):
         lst.append(randint(0, 10))
     my_print("before", lst)
-    return  bubble(lst)
+    return lst
 
 
 def bubble(ite):
     for i in range(len(ite)):
         for j in range(len(ite)):
-    	    if(ite[i] < ite[j]):
-               ite[i],ite[j] = ite[j],ite[i]
-    my_print("after", end=" " )
-    return  ite
-if __name__ == "__main__":
-    my_print(creating_list())
+            if (ite[i] < ite[j]):
+                ite[i], ite[j] = ite[j], ite[i]
+    my_print("after")
+    return ite
 
+
+if __name__ == "__main__":
+    my_print(bubble(creating_list()))
