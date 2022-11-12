ace78632aaf8227c03e60832e515c43fd5700ff4
Serojisa551 <116907048+Serojisa551@users.noreply.github.com>
2022-11-12 05:06:17 +0400

    bubble.py in this file I changed the name of the function imported by my_print from the file insertion.py , where it was written print, changed to my_printi.

--- sort/bubble.py
+++ sort/bubble.py
@@ -1,20 +1,21 @@
 from random import randint
+from insertion import my_print
 
 def creating_list():
     lst=[]
     for i in range(randint(2, 10)):
         lst.append(randint(0, 10))
-    print("before", lst)
-    return  bubbel(lst)
+    my_print("before", lst)
+    return  bubble(lst)
 
 
-def bubbel(ite):
+def bubble(ite):
     for i in range(len(ite)):
         for j in range(len(ite)):
     	    if(ite[i] < ite[j]):
                ite[i],ite[j] = ite[j],ite[i]
-    print("after", end=" " )
+    my_print("after", end=" " )
     return  ite
 if __name__ == "__main__":
-    print(creating_list())
+    my_print(creating_list())
 
