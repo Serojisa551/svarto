c03ab2eec4436db34fa601d4c58158f4df6ff55e
Serojisa551 <116907048+Serojisa551@users.noreply.github.com>
2022-11-16 00:09:25 +0400

    Wrote my own function my_print and removed all the functions print in the file linear

--- search/linear.py
+++ search/linear.py
@@ -1,16 +1,21 @@
 from random import randint
 
 
+def my_print(*object):
+    for element in object:
+        print(element)
+
+
 def creating_list():
     lst = []
     for i in range(randint(2, 10)):
         lst.append(randint(0, 10))
-    print(lst)
+    my_print(lst)
     return lst
 
 
 def linear(ite, key):
-    print(key)
+    my_print(key)
     for element in ite:
         if key == element:
             return True
@@ -19,4 +24,4 @@ def linear(ite, key):
 
 
 if __name__ == "__main__":
-    print(linear(creating_list(), randint(-2, 12)))
+    my_print(linear(creating_list(), randint(-2, 12)))
