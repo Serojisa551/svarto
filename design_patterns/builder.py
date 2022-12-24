7c25047e6afcaa715e345c0039340ea8deb668e1
Serojisa551 <116907048+Serojisa551@users.noreply.github.com>
2022-12-24 22:55:12 +0400

    Fixed errors

--- design_patterns/builder.py
+++ design_patterns/builder.py
@@ -1,28 +1,30 @@
 class BuilderCar:
-    def buildDoor(self, number = None):
-        # if number != None:
+    def buildDoor(self, number):
         self.door = number
-        # return self.door
+        return self
     
     def buildWheel(self, number):
         self.wheel = number
-        # return self.wheel
+        return  self
     
     def buildMotor(self, view):
         self.motor = view
-        # return self.motor
+        return self 
 
     def buildGps(self, availability):
         self.gps = availability
-        # return self.gps
+        return self
 
     def buildSpareWheel(self, availability):
         self.spareWheel = availability 
-        # return self.spareWheel 
+        return self
 
 
 bmw = BuilderCar()
 bmw = bmw.buildDoor(2)
-print(bmw)
+bmw = bmw.buildMotor("petrol")
+bmw = bmw.buildGps("network")
+bmw = bmw.buildWheel(4)
+print(bmw.door)
 # bmw = bmw.buildWheel(4)
 # print(bmw)
